36020c7a0df6da3a795128d74e476c5efb77aaab
Documenter.jl <documenter@juliadocs.github.io>
2021-06-24 23:02:14 +0000

    build based on 1278932

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TestGithub","category":"page"},{"location":"#TestGithub","page":"Home","title":"TestGithub","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TestGithub.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TestGithub]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TestGithub","category":"page"},{"location":"#TestGithub","page":"Home","title":"TestGithub","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TestGithub.","category":"page"},{"location":"","page":"Home","title":"Home","text":"This is the Test.","category":"page"}]
 }
