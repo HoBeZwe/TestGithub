c30a70d07ba504d8d7a1f5f54c46f352d92eddac
Bernd Hofmann <bernd.hofmann@tum.de>
2022-09-13 15:30:48 +0200

    supported versions

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,6 @@ using Test
 
 @testset "TestSomething" begin
     @test linearComb(1,2,3,0) == 3
-    @test linearComb(1,2,4,0) == 3
-    @test linearComb(1,2,4,0) == 3
+    @test linearComb(1,2,3,0) == 3
+    @test linearComb(1,2,3,0) == 3
 end
